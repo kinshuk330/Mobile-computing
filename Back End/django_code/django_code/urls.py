00138b90f2bc9034f139a8d129312b6bcacb33f6
kinshuk <kinshuk18239@iiitd.ac.in>
2022-03-18 11:14:24 +0530

    created barebones for backend

--- Back End/django_code/django_code/urls.py
+++ Back End/django_code/django_code/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls)
     ,path("about/",views.about) # gonna call this function when someone access this link
-    ,path(r"^$",views.homepage)
+    # ,path(r"^$",views.homepage),
+    ,path("",include("myapi.urls"))
 
 
 ]
