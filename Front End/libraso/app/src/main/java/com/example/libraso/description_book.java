6fe5dea75b374830fc0a5c914197dadf911f8345
Deadpool <nitin18251@iiitd.ac.in>
2022-03-22 23:41:17 +0530

    partial description done

--- Front End/libraso/app/src/main/java/com/example/libraso/description_book.java
+++ Front End/libraso/app/src/main/java/com/example/libraso/description_book.java
@@ -1,19 +1,58 @@
 package com.example.libraso;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class description_book extends Fragment {
-
+    Dialog dialog;
+    Button onHold;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        dialog = new Dialog(getContext());
         return inflater.inflate(R.layout.description_book, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        onHold = view.findViewById(R.id.onHold);
+        onHold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowPopup(view);
+            }
+        });
+    }
+
+    public void ShowPopup(View v){
+        dialog.setContentView(R.layout.issue_popup);
+        Button yes = (Button) dialog.findViewById(R.id.yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button no = (Button) dialog.findViewById(R.id.no);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 }
