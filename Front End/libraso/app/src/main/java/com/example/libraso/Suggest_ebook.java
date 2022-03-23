d5a1c2e279658571a0c3eb0ce38e01beaf423b46
mayanksaka <jaspreet18237@iiitd.ac.in>
2022-03-23 12:21:35 +0530

    suggestion ebook,complaint UI updated

--- Front End/libraso/app/src/main/java/com/example/libraso/Suggest_ebook.java
+++ Front End/libraso/app/src/main/java/com/example/libraso/Suggest_ebook.java
@@ -11,13 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Suggest_ebook extends Fragment {
     private Button submit;
     private EditText title;
     private EditText description;
     private RadioGroup r_group;
-    private RadioButton selected_type;
+    private String selected_type;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,32 @@ public class Suggest_ebook extends Fragment {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int selectedRadioButtonId = r_group.getCheckedRadioButtonId();
+                if (selectedRadioButtonId != -1){
+                    System.out.println(selectedRadioButtonId);
+                    if(selectedRadioButtonId==1){
+                        selected_type = "B";
+                        description.setHint("ISBN No: 12345678, Author Name: Mary Janes, Price: 5xxx, Edition: 2, description: xyz, etc.");}
+                    if(selectedRadioButtonId==2){
+                        selected_type="J";
+                        description.setHint("Article Abstract: xyz, Published Author : Mary Janes, Date of Release: 02-05-1991 , URL: www.xyz.com");}
+
+
+                } else {
+                    Toast.makeText(getContext(),"Please select type",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(title.getText().toString().trim()==null || description.getText().toString().trim()==null){
+                    Toast.makeText(getContext(),"Please fill all details",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                Toast.makeText(getContext(),"Suggestion filled",Toast.LENGTH_SHORT).show();
+                title.setText("");
+                description.setText("");
+                description.setHint("description");
+                r_group.clearCheck();
 
             }
         });
