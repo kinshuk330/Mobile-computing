08afc42418ff247a4b71dbabc79a5fc98afbaa3e
kinshuk <kinshuk18239@iiitd.ac.in>
2022-05-13 05:51:52 +0530

    added show complaints resolve button needs work

--- Front End/libraso/app/src/main/java/com/example/libraso/Admin/complaints/complaint_adapter.java
+++ Front End/libraso/app/src/main/java/com/example/libraso/Admin/complaints/complaint_adapter.java
@@ -2,6 +2,7 @@ package com.example.libraso.Admin.complaints;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +13,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.libraso.Events.Event;
 import com.example.libraso.Events.event_adapter;
 import com.example.libraso.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class complaint_adapter extends RecyclerView.Adapter<complaint_adapter.ViewHolder>
 
@@ -31,28 +45,86 @@ public class complaint_adapter extends RecyclerView.Adapter<complaint_adapter.Vi
     @NonNull
     @Override
     public complaint_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.event_grid,parent,false);
+        View v = LayoutInflater.from(context).inflate(R.layout.complaint_grid,parent,false);
         return new complaint_adapter.ViewHolder(v);
 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull complaint_adapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+    public void onBindViewHolder(@NonNull complaint_adapter.ViewHolder holder, int position) {
         holder.complaint_title.setText("Title:"+complaint_list.get(position).getTitle());
         holder.complaint_name.setText("Name:"+complaint_list.get(position).getUsername());
         holder.complaint_description.setText("Description:"+complaint_list.get(position).getDescritption());
         holder.complaint_resolve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                update_complaint(complaint_list.get(position));
+
                 complaint_list.remove(position);
                 notifyDataSetChanged();
-                update_complaint();
             }
         });
     }
 
-    private void update_complaint() {
+    private void update_complaint(complaints_class temp) {
 
+
+
+        RequestQueue queue = Volley.newRequestQueue(context.getApplicationContext());  // this = context
+        JSONObject params = new JSONObject();
+        try {
+            //input your API parameters
+            params.put("id", String.valueOf(temp.id));
+            params.put("title", temp.getTitle());
+            params.put("description", temp.getDescritption());
+            params.put("status", "true");
+            params.put("user_id", String.valueOf(temp.getUserid()));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String  url = "https://libraso.herokuapp.com/complaint/resolve/"+String.valueOf(temp.id);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url,params,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Response", response.toString());
+
+                    }
+
+
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        String body= null;
+                        try {
+                            body = new String(error.networkResponse.data,"UTF-8");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ) {
+
+//            @Override
+//            protected Map<String, String> getParams()
+//            {
+//                Map<String, String>  params = new HashMap<String, String>();
+//                params.put("id", String.valueOf(temp.id));
+//                params.put("title", temp.getTitle());
+//                params.put("description", temp.getDescritption());
+//                params.put("status", "true");
+//                params.put("user_id", String.valueOf(temp.getUserid()));
+//                System.out.println(params.);
+//                return params;
+//            }
+
+        };
+
+        queue.add(jsonObjectRequest);
     }
 
     @Override
